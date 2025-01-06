91cf7d30bfbb23496aa4c589efed0434f3793e0d
wksk1125 <wksk1125@gmail.com>
2025-01-06 10:57:54 +0900

    Initial commit

---
+++ src/main/java/wksk1125/hello_springboot_app/controller/HelloController.java
@@ -0,0 +1,13 @@
+package wksk1125.hello_springboot_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!!";
+    }
+}
---
+++ src/test/java/wksk1125/hello_springboot_app/HelloSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package wksk1125.hello_springboot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringbootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
